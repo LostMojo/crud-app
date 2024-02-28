04ee508c2cdb2228af6a573b99332adeb271f237
Mary Jo G <mj81000@gmail.com>
2024-02-28 12:33:33 -0600

    improves form validation

--- src/main/resources/static/js/clientValidator.js
+++ src/main/resources/static/js/clientValidator.js
@@ -1,51 +1,70 @@
-
-const formInputs = [
+const validationRules = [
     {
         name: "companyName",
-        rule: /^.{1,50}$/
+        rule: /^.{1,50}$/,
+        ruleDescription: "Company name is required with a maximum length of 50"
     },
     {
         name: "websiteUri",
-        rule: /^.{1,100}$/
+        rule: /^.{1,100}$/,
+        ruleDescription: "Website URI is required with a maximum length of 100"
     },
     {
         name: "phoneNumber",
-        rule: /^[0-9]{9,12}$/
+        rule: /^[0-9]{9,12}$/,
+        ruleDescription: "Phone number must be between 9 and 12 digits. No spaces or special characters."
     },
     {
         name: "streetAddress",
-        rule: /^.{1,50}$/
+        rule: /^.{1,50}$/,
+        ruleDescription: "Street address is required with a maximum length of 50."
     },
     {
         name: "city",
-        rule: /^.{1,50}$/
+        rule: /^.{1,50}$/,
+        ruleDescription: "City is required with maximum length of 50."
     },
     {
         name: "state",
-        rule: /^.{1,2}$/
+        rule: /^.{2}$/,
+        ruleDescription: "State is required with length 2."
     },
     {
         name: "zipCode",
-        rule: /^.{1,5}$/
+        rule: /^.{5}$/,
+        ruleDescription: "Zip code is required with length 5."
     },
 ];
 
-formInputs.forEach((formInput) => {
-    document.getElementsByName(formInput.name)[0].addEventListener("change", function() {validateChange(formInput)});
-});
+const displayErrorMessage = (validationRule, inputElement) => {
+    inputName = validationRule.name;
 
-function validateChange(formInput){
-    let inputElement = document.getElementsByName(formInput.name)[0];
-    let inputGroup = inputElement.parentElement;
+    if (document.getElementById(`${inputName}-error`))
+        return
 
-    if (inputElement.value.match(formInput.rule)) {
-        inputGroup.classList.remove("has-error");
-        if (!document.getElementsByClassName("has-error")[0]) 
-            document.querySelector("input[name='Submit']").disabled = false;
-        return true;
-    } else {
-        inputGroup.classList.add("has-error");
-        document.querySelector("input[name='Submit']").disabled = true;
-        return false;
-    }
+    const inputGroup = inputElement.parentElement;
+
+    const errorMessageElement = document.createElement("div");
+    errorMessageElement.id = `${inputName}-error`;
+    errorMessageElement.classList.add("text-danger");
+    
+    const errorMessageText =  document.createTextNode(validationRule.ruleDescription);
+    errorMessageElement.appendChild(errorMessageText);
+
+    inputGroup.appendChild(errorMessageElement);
 }
+
+const removeErrorMessage = (inputElement) => {
+    const errorMessageElement = document.querySelector(`#${inputElement.name}-error`);
+    if (errorMessageElement)
+        errorMessageElement.remove();
+}
+
+function addValidateFormListeners(validatorRules) {
+    validatorRules.forEach((validatorRule) => {
+        document.getElementsByName(validatorRule.name)[0].addEventListener("change",  () => validateChange(validatorRule));
+    });
+}
+
+
+addValidateFormListeners(personValidationRules);
--- src/main/resources/static/js/personValidator.js
+++ src/main/resources/static/js/personValidator.js
@@ -1,57 +1,89 @@
 
-const formInputs = [
+const personValidationRules = [
     {
         name: "firstName",
-        rule: /^.{1,50}$/
+        rule: /^.{1,50}$/,
+        ruleDescription: "First name is required with a maximum length of 50."
     },
     {
         name: "lastName",
-        rule: /^.{1,50}$/
+        rule: /^.{1,50}$/,
+        ruleDescription: "Last name is required with a maximum length of 50."
     },
     {
         name: "emailAddress",
-        rule: /^[a-z0-9._%+-]+@[a-z0-9.-]+$/
+        rule: /^[a-z0-9._%+-]+@[a-z0-9.-]+$/,
+        ruleDescription: "Email is required, must contain an @"
     },
     {
         name: "streetAddress",
-        rule: /^.{1,50}$/
+        rule: /^.{1,50}$/,
+        ruleDescription: "Street address is required with a maximum length of 50."
     },
     {
         name: "city",
-        rule: /^.{1,50}$/
+        rule: /^.{1,50}$/,
+        ruleDescription: "City is required with maximum length of 50."
     },
     {
         name: "state",
-        rule: /^.{1,2}$/
+        rule: /^.{2}$/,
+        ruleDescription: "State is required with length 2."
     },
     {
         name: "zipCode",
-        rule: /^.{1,5}$/
+        rule: /^.{5}$/,
+        ruleDescription: "Zip code is required with length 5."
     },
 ];
 
-formInputs.forEach((formInput) => {
-    document.getElementsByName(formInput.name)[0].addEventListener("change", function() {validateChange(formInput)});
-});
+const validateChange = (validationRule) => {
+    const inputElement = document.getElementsByName(validationRule.name)[0];
+    const inputGroup = inputElement.parentElement;
 
-
-
-//test!
-document.getElementsByTagName("nav")[0].classList.add("test2");
-
-
-function validateChange(formInput){
-    let inputElement = document.getElementsByName(formInput.name)[0];
-    let inputGroup = inputElement.parentElement;
-
-    if (inputElement.value.match(formInput.rule)) {
+    if (inputElement.value.match(validationRule.rule)) {
         inputGroup.classList.remove("has-error");
+        removeErrorMessage(inputElement);
         if (!document.getElementsByClassName("has-error")[0]) 
             document.querySelector("input[name='Submit']").disabled = false;
         return true;
     } else {
         inputGroup.classList.add("has-error");
+        displayErrorMessage(validationRule, inputElement);
         document.querySelector("input[name='Submit']").disabled = true;
         return false;
     }
 }
+
+const displayErrorMessage = (validationRule, inputElement) => {
+    inputName = validationRule.name;
+
+    if (document.getElementById(`${inputName}-error`))
+        return
+
+    const inputGroup = inputElement.parentElement;
+
+    const errorMessageElement = document.createElement("div");
+    errorMessageElement.id = `${inputName}-error`;
+    errorMessageElement.classList.add("text-danger");
+    
+    const errorMessageText =  document.createTextNode(validationRule.ruleDescription);
+    errorMessageElement.appendChild(errorMessageText);
+
+    inputGroup.appendChild(errorMessageElement);
+}
+
+const removeErrorMessage = (inputElement) => {
+    const errorMessageElement = document.querySelector(`#${inputElement.name}-error`);
+    if (errorMessageElement)
+        errorMessageElement.remove();
+}
+
+function addValidateFormListeners(validatorRules) {
+    validatorRules.forEach((validatorRule) => {
+        document.getElementsByName(validatorRule.name)[0].addEventListener("change",  () => validateChange(validatorRule));
+    });
+}
+
+
+addValidateFormListeners(personValidationRules);
